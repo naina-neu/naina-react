5e72e107ad8a39fc7cc0ceedc7265643401abe0c
Naina Rajan <rajan.nai@northeastern.edu>
2023-08-22 13:10:28 -0400

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 /**
  * <div id="parent">
  *      <div id="child">
@@ -12,10 +15,9 @@
  *
  * ReactElement(Object) => HTML(Browser Understands)
  */
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I' m an H1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I' m an H2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
